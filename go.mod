a97990408fb55d634150ec3e4ca95513360615bd
Stephen L. McMahon <smcmahon@slb.com>
2023-12-17 08:27:17 -0500

    Updated to use shared libraries

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module slmcmahon.com/varcompare
 
-go 1.21.1
+go 1.21.5
+
+require (
+	github.com/slmcmahon/go-azdo v0.0.0-20231217132327-b7cf3a3fdbd8 // indirect
+	github.com/slmcmahon/go-common v0.0.0-20231217131042-f828f497f218 // indirect
+)
--- main.go
+++ main.go
@@ -7,18 +7,10 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-)
 
-const (
-	org     = "slb-it"
-	project = "es-competency-management-system"
+	"github.com/slmcmahon/go-azdo"
+	slmcommon "github.com/slmcmahon/go-common"
 )
-
-type User struct {
-	DisplayName string `json:"displayName"`
-	ID          string `json:"id"`
-}
 
 type Variable struct {
 	Value string `json:"value"`
@@ -32,38 +24,6 @@ type VariableGroup struct {
 type VariableGroupsResponse struct {
 	Count int             `json:"count"`
 	Value []VariableGroup `json:"value"`
-}
-
-// CompareAndPrintDifference compares two variable groups and prints variables that are in the first group but not in the second.
-func CompareAndPrintDifference(group1Name string, group1Vars map[string]struct{}, group2Name string, group2Vars map[string]struct{}) {
-	var hasDifference bool
-
-	fmt.Printf("\nVariables in %s but not in %s:\n", group1Name, group2Name)
-	for name := range group1Vars {
-		if _, exists := group2Vars[name]; !exists {
-			fmt.Println(" - " + name)
-			hasDifference = true
-		}
-	}
-
-	if !hasDifference {
-		fmt.Println(" - No differences found")
-	}
-}
-
-// checkEnvOrFlag checks if the command-line flag is set; if not, it checks for an environment variable.
-// If neither is set, it logs a fatal error.
-func checkEnvOrFlag(flagValue, envVarName string) string {
-	if flagValue != "" {
-		return flagValue
-	}
-
-	envValue, exists := os.LookupEnv(envVarName)
-	if !exists {
-		log.Fatalf("No value was provided for '%s'.\n\nEither provide it as a command-line argument or set an environment variable called '%s'.\n\nExiting.", envVarName, envVarName)
-	}
-
-	return envValue
 }
 
 func main() {
@@ -82,9 +42,18 @@ func main() {
 	flag.StringVar(&projectFlag, "project", "", "Azure DevOps Project")
 	flag.Parse()
 
-	pat := checkEnvOrFlag(patFlag, "AZDO_PAT")
-	org := checkEnvOrFlag(orgFlag, "AZDO_ORG")
-	project := checkEnvOrFlag(projectFlag, "AZDO_PROJECT")
+	pat, err := slmcommon.CheckEnvOrFlag(patFlag, "AZDO_PAT")
+	if err != nil {
+		log.Fatal(err)
+	}
+	org, err := slmcommon.CheckEnvOrFlag(orgFlag, "AZDO_ORG")
+	if err != nil {
+		log.Fatal(err)
+	}
+	project, err := slmcommon.CheckEnvOrFlag(projectFlag, "AZDO_PROJECT")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	url := fmt.Sprintf("https://dev.azure.com/%s/%s/_apis/distributedtask/variablegroups?groupIds=%d,%d&api-version=6.0-preview.2", org, project, lib1, lib2)
 	client := &http.Client{}
@@ -126,6 +95,6 @@ func main() {
 		variablesInGroup2[name] = struct{}{}
 	}
 
-	CompareAndPrintDifference(group1.Name, variablesInGroup1, group2.Name, variablesInGroup2)
-	CompareAndPrintDifference(group2.Name, variablesInGroup2, group1.Name, variablesInGroup1)
+	azdo.CompareAndPrintDifference(group1.Name, variablesInGroup1, group2.Name, variablesInGroup2)
+	azdo.CompareAndPrintDifference(group2.Name, variablesInGroup2, group1.Name, variablesInGroup1)
 }
